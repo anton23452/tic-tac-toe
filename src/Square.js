790b96b23fa6b2d3cfbee25552f9e339185f5fc2
anton23452 <antonosmyhin@gmail.com>
2024-05-19 00:08:05 +0300

    Initial commit

---
+++ src/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Square({ value, onSquareClick, isWinning }) {
+  return (
+    <button className={`square ${isWinning ? 'winning' : ''}`} onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
